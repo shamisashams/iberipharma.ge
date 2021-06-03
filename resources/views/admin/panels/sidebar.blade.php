5e03e1c695cc591761a07709cc555e8c99c024d7
makhatadze <vitomaxatadze@gmail.com>
2021-06-03 16:55:56 +0400

    Show language sidebar.

--- resources/views/admin/panels/sidebar.blade.php
+++ resources/views/admin/panels/sidebar.blade.php
@@ -54,7 +54,7 @@
       <li class="bold {{(request()->is($menu->url.'*')) ? 'active' : '' }}">
         <a class="{{$custom_classes}} {{ (request()->is($menu->url.'*')) ? 'active '.$configData['activeMenuColor'] : ''}}"
           @if(!empty($configData['activeMenuColor'])) {{'style=background:none;box-shadow:none;'}} @endif
-          href="@if(($menu->url)==='javascript:void(0)'){{$menu->url}} @else{{url($menu->url)}} @endif"
+          href="@if(($menu->url)==='javascript:void(0)'){{$menu->url}} @else{{route($menu->url)}} @endif"
           {{isset($menu->newTab) ? 'target="_blank"':''}}>
           <i class="material-icons">{{$menu->icon}}</i>
           <span class="menu-title">{{ __('locale.'.$menu->name)}}</span>
