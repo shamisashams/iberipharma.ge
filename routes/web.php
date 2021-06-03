85400f4d854ac3421817224768d23f8888dfffb2
makhatadze <vitomaxatadze@gmail.com>
2021-06-03 16:05:50 +0400

    Register helper file into composer.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
+/**
+ *  routes/web.php
+ *
+ * Date-Time: 03.06.21
+ * Time: 15:41
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
 
+use App\Http\Controllers\Admin\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('admin')->group(function () {
+    Route::get('login', [LoginController::class, 'loginView'])->name('loginView');
+    Route::post('login', [LoginController::class, 'login'])->name('login');
 });
