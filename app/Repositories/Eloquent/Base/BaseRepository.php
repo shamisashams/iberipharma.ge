46cdc665ab8b10bceb49312b0f25529d44d5ddbf
makhatadze <vitomaxatadze@gmail.com>
2021-06-04 09:42:01 +0400

    Add base repository and EloquentRepositoryInterface.

---
+++ app/Repositories/Eloquent/Base/BaseRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  app/Repositories/Eloquent/Base/BaseRepository.php
+ *
+ * Date-Time: 04.06.21
+ * Time: 09:41
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Repositories\Eloquent\Base;
+
+/**
+ * Class BaseRepository
+ * @package App\Repositories\Eloquent\Base
+ */
+class BaseRepository implements EloquentRepositoryInterface
+{
+
+}
---
+++ app/Repositories/Eloquent/Base/EloquentRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  app/Repositories/Eloquent/Base/EloquentRepositoryInterface.php
+ *
+ * Date-Time: 04.06.21
+ * Time: 09:41
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Repositories\Eloquent\Base;
+
+/**
+ * Interface EloquentRepositoryInterface
+ * @package App\Repositories\Eloquent\Base
+ */
+interface EloquentRepositoryInterface
+{
+
+
+}
