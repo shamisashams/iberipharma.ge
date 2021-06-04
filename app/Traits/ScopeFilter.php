3510efbffff9b5018f13a795df44bc9c2c971813
makhatadze <vitomaxatadze@gmail.com>
2021-06-04 10:11:15 +0400

    Make ScopeFilter trait for global filter all models.

---
+++ app/Traits/ScopeFilter.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ *  app/Traits/ScopeFilter.php
+ *
+ * Date-Time: 04.06.21
+ * Time: 10:05
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait ScopeFilter
+{
+
+    /**
+     * @param  $request
+     *
+     * @return Builder
+     */
+    public function filter($request): Builder
+    {
+        $data = $this->query();
+        $filterScopes = $this->getFilterScopes();
+        foreach ($this->getActiveFilters($request) as $filter => $value) {
+            if (!array_key_exists($filter, $filterScopes)) {
+                continue;
+            }
+            $filterScopeData = $filterScopes[$filter];
+
+            if (false === $filterScopeData['hasParam']) {
+                $data->{$value}();
+                continue;
+            }
+            $methodToExecute = $filterScopeData['scopeMethod'];
+            $data->{$methodToExecute}($value);
+        }
+
+        $sortParams = ['sort' => 'id', 'order' => 'desc'];
+
+        if ($request->filled('sort') && $request->filled('order')) {
+            $sortParams = $request->only('sort', 'order');
+        }
+
+        return $data->sorted($sortParams);
+    }
+
+    /**
+     * @param  $request
+     *
+     * @return array
+     */
+    public function getActiveFilters($request): array
+    {
+        $activeFilters = [];
+        foreach ($this->getFilterScopes() as $key => $value) {
+            if ($request->filled($key)) {
+                $activeFilters [$key] = $request->{$key};
+            }
+        }
+        return $activeFilters;
+    }
+
+    /**
+     * @param $query
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function scopeId($query, $id)
+    {
+        return $query->where('id', $id);
+    }
+
+    /**
+     * @param $query
+     * @param $name
+     *
+     * @return mixed
+     */
+    public function scopeName($query, $name)
+    {
+        return $query->where('name', 'like', '%' . $name . '%');
+    }
+
+    /**
+     * @param $query
+     * @param $name
+     *
+     * @return mixed
+     */
+    public function scopeTitle($query, $title)
+    {
+        return $query->where('title', 'like', '%' . $title . '%');
+    }
+
+    /**
+     * @param $query
+     * @param $status
+     *
+     * @return mixed
+     */
+    public function scopeStatus($query, $status)
+    {
+        return $query->where('status', $status);
+    }
+}
