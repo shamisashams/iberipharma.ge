3db8954390f51cc9d68949af937128af6c3e22cb
makhatadze <vitomaxatadze@gmail.com>
2021-06-04 09:44:30 +0400

    Make RepositoryServiceProvider and add to config file.

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *  app/Providers/RepositoryServiceProvider.php
+ *
+ * Date-Time: 04.06.21
+ * Time: 09:42
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Providers;
+
+use App\Repositories\Eloquent\Base\BaseRepository;
+use App\Repositories\Eloquent\Base\EloquentRepositoryInterface;
+use Carbon\Laravel\ServiceProvider;
+
+/**
+ * Class RepositoryServiceProvider
+ * @package App\Providers
+ */
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
+    }
+}
