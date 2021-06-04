949e01e1ccc5fabe0271549a58ccaba11731e200
makhatadze <vitomaxatadze@gmail.com>
2021-06-04 10:12:25 +0400

    Add LanguageRepositoryInterface get data function.

---
+++ app/Repositories/Eloquent/LanguageRepository.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *  app/Repositories/Eloquent/LanguageRepository.php
+ *
+ * Date-Time: 04.06.21
+ * Time: 09:55
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Repositories\Eloquent;
+
+
+use App\Repositories\Eloquent\Base\BaseRepository;
+use App\Repositories\LanguageRepositoryInterface;
+
+/**
+ * Class LanguageRepository
+ * @package App\Repositories\Eloquent
+ */
+class LanguageRepository extends BaseRepository implements LanguageRepositoryInterface
+{
+
+}
