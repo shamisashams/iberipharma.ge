40fbc3db87bd0c164c2381eb3fe42a1e1308b1c3
makhatadze <vitomaxatadze@gmail.com>
2021-06-07 16:05:03 +0400

    Add categories migration.

---
+++ database/migrations/2021_06_07_115648_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *  database/migrations/2021_06_07_115648_create_categories_table.php
+ *
+ * Date-Time: 07.06.21
+ * Time: 15:57
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->string('position')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
