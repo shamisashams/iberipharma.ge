9a20d9205b63f146a6b83adf1af498c13df05f4e
makhatadze <vitomaxatadze@gmail.com>
2021-06-07 16:56:59 +0400

    Add category model with methods.

---
+++ app/Models/Category.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *  app/Models/Category.php
+ *
+ * Date-Time: 07.06.21
+ * Time: 15:56
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Models;
+
+use App\Traits\ScopeFilter;
+use Collective\Html\Eloquent\FormAccessible;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Category
+ * @package App\Models
+ * @property integer $id
+ * @property integer $parent_id
+ * @property string $slug
+ * @property string $position
+ * @property boolean $status
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $deleted_at
+ */
+class Category extends Model
+{
+    use HasFactory, softDeletes, ScopeFilter;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'slug',
+        'position',
+        'status'
+    ];
+
+    public function getFilterScopes(): array
+    {
+        return [
+            'id' => [
+                'hasParam' => true,
+                'scopeMethod' => 'id'
+            ],
+            'slug' => [
+                'hasParam' => true,
+                'scopeMethod' => 'slug'
+            ],
+            'active' => [
+                'hasParam' => true,
+                'scopeMethod' => 'active'
+            ]
+        ];
+    }
+}
