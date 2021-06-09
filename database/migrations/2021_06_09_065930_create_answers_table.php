5700fe18308f864e54c0e6b44db5d2ec71eac3c0
makhatadze <vitomaxatadze@gmail.com>
2021-06-09 11:01:33 +0400

    Add answer migration.

---
+++ database/migrations/2021_06_09_065930_create_answers_table.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  database/migrations/2021_06_09_065930_create_answers_table.php
+ *
+ * Date-Time: 09.06.21
+ * Time: 10:59
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->string('position')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
