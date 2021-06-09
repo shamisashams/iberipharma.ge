80a17440f85e98c18ee422e2dfb1effea5ec3369
makhatadze <vitomaxatadze@gmail.com>
2021-06-09 16:15:02 +0400

    Add projectRequest for validation.

---
+++ app/Http/Requests/Admin/ProjectRequest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  app/Http/Requests/Admin/ProjectRequest.php
+ *
+ * Date-Time: 09.06.21
+ * Time: 16:14
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class ProjectRequest
+ * @package App\Http\Requests\Admin
+ */
+class ProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
