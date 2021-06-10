d799ffa02fbb37bd13b4fa67c6964b5cb5189344
makhatadze <vitomaxatadze@gmail.com>
2021-06-10 11:06:59 +0400

    Make Product model.

---
+++ app/Models/Product.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *  app/Models/Product.php
+ *
+ * Date-Time: 10.06.21
+ * Time: 11:04
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+
+namespace App\Models;
+
+use App\Traits\ScopeFilter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Product
+ * @package App\Models
+ * @property integer $id
+ * @property integer $category_id
+ * @property string $slug
+ * @property boolean $status
+ * @property integer $price
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $deleted_at
+ */
+class Product extends Model
+{
+    use HasFactory, softDeletes, ScopeFilter;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'slug',
+        'status',
+        'price'
+    ];
+}
