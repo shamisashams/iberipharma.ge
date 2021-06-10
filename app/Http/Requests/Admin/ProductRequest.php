67cbbf2572c21ef4264802ee110d26dca5c7ccb4
makhatadze <vitomaxatadze@gmail.com>
2021-06-10 15:07:35 +0400

    Add product Request.

---
+++ app/Http/Requests/Admin/ProductRequest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  app/Http/Requests/Admin/ProductRequest.php
+ *
+ * Date-Time: 10.06.21
+ * Time: 15:07
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class ProductRequest
+ * @package App\Http\Requests\Admin
+ */
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
