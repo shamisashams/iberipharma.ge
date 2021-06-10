ff41dd85da13c45a754f56877e886b42aaf2ce43
makhatadze <vitomaxatadze@gmail.com>
2021-06-10 10:48:08 +0400

    Create products migration.

---
+++ database/migrations/2021_06_10_064539_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *  database/migrations/2021_06_10_064539_create_products_table.php
+ *
+ * Date-Time: 10.06.21
+ * Time: 10:46
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->boolean('status')->default(true);
+            $table->bigInteger('price');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
