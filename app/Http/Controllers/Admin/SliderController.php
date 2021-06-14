8737777eb88eb6180ab10014489392579d4af343
makhatadze <vitomaxatadze@gmail.com>
2021-06-14 15:51:47 +0400

    Show Slider module.

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -6,6 +6,7 @@
  * Time: 15:31
  * @author Vito Makhatadze <vitomaxatadze@gmail.com>
  */
+
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
@@ -30,7 +31,8 @@ class SliderController extends Controller
      *
      * @param \App\Repositories\SliderRepositoryInterface $sliderRepository
      */
-    public function __construct(SliderRepositoryInterface $sliderRepository) {
+    public function __construct(SliderRepositoryInterface $sliderRepository)
+    {
         $this->sliderRepository = $sliderRepository;
     }
 
@@ -41,8 +43,8 @@ class SliderController extends Controller
      */
     public function index(SliderRequest $request)
     {
-        return view('admin.pages.slider.index',[
-           'sliders' => $this->sliderRepository->getData($request,['languages']),
+        return view('admin.pages.slider.index', [
+            'sliders' => $this->sliderRepository->getData($request, ['languages']),
             'languages' => $this->activeLanguages()
         ]);
     }
@@ -50,39 +52,73 @@ class SliderController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        $slider = $this->sliderRepository->model;
+
+        $url = locale_route('slider.store', [], false);
+        $method = 'POST';
+
+        return view('admin.pages.slider.form', [
+            'slider' => $slider,
+            'url' => $url,
+            'method' => $method,
+            'languages' => $this->activeLanguages(),
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \App\Http\Requests\Admin\SliderRequest $request
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function store(Request $request)
+    public function store(SliderRequest $request)
     {
-        //
+        $data = [
+            'url' => $request['url'],
+            'status' => (bool)$request['status'],
+            'title' => $request['title'],
+            'description' => $request['description'],
+            'languages' => $this->activeLanguages(),
+        ];
+
+        $slider = $this->sliderRepository->create($data);
+
+        // Save Files
+        if ($request->hasFile('images')) {
+            $slider = $this->sliderRepository->saveFiles($slider->id, $request);
+        }
+
+        return redirect(locale_route('slider.show', $slider->id))->with('success', 'Slider created.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param string $locale
+     * @param int $id
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
-    public function show($id)
+    public function show(string $locale, int $id)
     {
-        //
+        $slider = $this->sliderRepository->findOrFail($id);
+
+        return view('admin.pages.slider.show', [
+            'slider' => $slider,
+            'languages' => $this->activeLanguages()
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -93,8 +129,9 @@ class SliderController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -105,7 +142,8 @@ class SliderController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
