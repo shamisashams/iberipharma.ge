b4c80c2548962eb7bb75b3ae1ca3e760c1ffd2fc
makhatadze <vitomaxatadze@gmail.com>
2021-06-18 11:37:04 +0400

    Show index page.

---
+++ app/Http/Controllers/Client/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        return view('client.pages.home.index');
+    }
+}
