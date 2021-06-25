0032257b2aef14e819afd83fff0180c80af65cd3
makhatadze <vitomaxatadze@gmail.com>
2021-06-25 15:21:06 +0400

    Add logo.

--- resources/views/client/layout/partial/header.blade.php
+++ resources/views/client/layout/partial/header.blade.php
@@ -1,7 +1,6 @@
 <header class="header">
     <div class="header_content flex wrapper">
         <a href="{{locale_route('home.index')}}" class="logo">
-            <img style="width: 100px" src="/client/img/logo/logo.png" alt=""/>
         </a>
         <div class="navbar">
             <a href="{{locale_route('home.index')}}"
