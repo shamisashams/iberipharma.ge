0f8aee6f1f26351bffb3037b81b93353a547c9db
makhatadze <vitomaxatadze@gmail.com>
2021-06-25 15:15:24 +0400

    Add logo.

--- resources/views/client/layout/site.blade.php
+++ resources/views/client/layout/site.blade.php
@@ -6,7 +6,7 @@
             type="text/css"
             href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"
     />
-    <link rel="stylesheet" href="{{ asset('../client/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('../client/style.css?v=2') }}" />
     @yield('subhead')
 @endsection
 
